ecfbdcf6850db5b6799048e45b980cb290c9c8de
vickyruud <aprajit.k@gmail.com>
2022-07-18 12:27:29 -0400

    add styles to modal

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -22,7 +22,14 @@ export default function GoalInput({ onAddGoal, visible }) {
           placeholder="Your Goal!"
           value={enteredGoalText}
         />
-        <Button onPress={addGoalHandler} title="Add Goal" />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title="Add Goal" onPress={addGoalHandler} />
+          </View>
+          <View style={styles.button}>
+            <Button title="Cancel" />
+          </View>
+        </View>
       </View>
     </Modal>
   );
@@ -31,18 +38,26 @@ export default function GoalInput({ onAddGoal, visible }) {
 const styles = StyleSheet.create({
   inputContainer: {
     flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: "column",
+    justifyContent: "center",
     alignItems: "center",
     marginBottom: 24,
+    padding: 16,
     borderBottomWidth: 1,
     borderBottomColor: "#234FC7",
   },
   textInput: {
     borderWidth: 1,
     borderColor: "#234FC7",
-    width: "70%",
-    marginRight: 8,
+    width: "100%",
     padding: 8,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    marginTop: 16,
+  },
+  button: {
+    width: 100,
+    marginHorizontal: 8,
   },
 });
